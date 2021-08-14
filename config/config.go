25fcf30104a54f00c414db4e7ea467442d6fe686
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2021-08-14 23:53:47 +0900

    create new config.ini

---
+++ config/config.go
@@ -0,0 +1,28 @@
+package config
+
+import (
+	"log"
+	"os"
+
+	"gopkg.in/ini.v1"
+)
+
+type ConfigList struct {
+	ApiKey    string
+	ApiSecret string
+}
+
+var Config ConfigList
+
+func init() {
+	cfg, err := ini.Load("config.ini")
+	if err != nil {
+		log.Printf("Failed to read file: %v", err)
+		os.Exit(1)
+	}
+
+	Config = ConfigList{
+		ApiKey:    cfg.Section("bitflyer").Key("api_key").String(),
+		ApiSecret: cfg.Section("bitflyer").Key("api_secret").String(),
+	}
+}

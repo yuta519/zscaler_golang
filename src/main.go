1ccbd1230ce9db70e834b2388e2a5a5b67eabdb3
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2021-09-01 21:44:39 +0900

    using FetchAllUrlCategories from main.go

--- src/main.go
+++ src/main.go
@@ -1,51 +1,15 @@
 package main
 
 import (
-	"bytes"
-	"encoding/json"
 	"fmt"
-	"net/http"
-	"net/url"
-	"strconv"
-	"zscaler_golang/auth"
-	"zscaler_golang/config"
+	"zscaler_golang/lib"
 )
 
-type Payload struct {
-	APIKey    string `json:"apiKey"`
-	Username  string `json:"username"`
-	Password  string `json:"password"`
-	Timestamp string `json:"timestamp"`
-}
-
-func login(
-	hostname string, api_key string, username string, password string, timestamp int,
-) {
-	base, _ := url.Parse("https://" + hostname)
-	reference, _ := url.Parse("/api/v1/authenticatedSession")
-	endpoint := base.ResolveReference(reference).String()
-
-	payload := new(Payload)
-	payload.APIKey = api_key
-	payload.Username = username
-	payload.Password = password
-	payload.Timestamp = strconv.Itoa(timestamp)
-	payload_json, _ := json.Marshal(payload)
-
-	res, err := http.Post(endpoint, "application/json", bytes.NewBuffer(payload_json))
-	if err != nil {
-		fmt.Println("Request Error: ", err)
-		return
-	}
-	defer res.Body.Close()
-	fmt.Println(res)
-}
-
 func main() {
-	api_key := auth.Auth.ObfuscatedApiKey
-	timestamp := auth.Auth.Timestamp
-	hostname := config.Config.Hostname
-	username := config.Config.UserName
-	password := config.Config.Password
-	login(hostname, api_key, username, password, timestamp)
+	url_categories := lib.FetchAllUrlCategories()
+	fmt.Print(url_categories)
+	// target_urls := []string["aaa.com", "bbb.com"]
+	// target_urls := []string{"aaa.com", "bbb.com"}
+	// category := lib.LookupUrlCategory(target_urls)
+	// fmt.Print(category)
 }

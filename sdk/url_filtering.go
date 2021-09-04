b8364b9daa450c03ee8d0c320813bd8c3b69599d
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2021-09-04 11:25:19 +0900

    create new file url_filtering.go

---
+++ sdk/url_filtering.go
@@ -0,0 +1,40 @@
+package sdk
+
+import (
+	"encoding/json"
+	"fmt"
+)
+
+type UrlFilteringRule struct {
+	Id        int      `json:"id"`
+	Name      string   `json:"name"`
+	Order     int      `json:"order"`
+	Protocol  []string `json:"protocol"`
+	Locations []string `json:"locations"`
+	// TODO: later...
+}
+
+func FetchAllUrlFilteringRules() []UrlFilteringRule {
+	session_id := Login()
+	is_full := true
+	response := GetApi(endpoint_rul_categories(is_full), session_id)
+	Logout()
+	var url_categories []UrlCategory
+	json.Unmarshal(response, &url_categories)
+	for i := range url_categories {
+		fmt.Println(url_categories[i].Id)
+	}
+	var rules []UrlFilteringRule
+	return rules
+}
+
+func FetchSpecifiedUrlFilteringRule() UrlFilteringRule {
+	var rule UrlFilteringRule
+	return rule
+}
+
+func CreateUrlFilteringRule() {
+}
+
+func UpdateFilteringRule() {
+}

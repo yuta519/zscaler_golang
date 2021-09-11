1afab4701b7fb9d3b00e3224a89e3d3d80fd5b24
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2021-09-11 15:58:59 +0900

    add how to create admin user

--- src/main.go
+++ src/main.go
@@ -6,12 +6,31 @@ import (
 )
 
 func main() {
-	urlcategories := pkg.FetchAllUrlCategories()
-	fmt.Print(urlcategories)
+	// Show Admin Users
+	adminusers := pkg.CreateAdminUsers(
+		"Super Admin",
+		"yuta.kawamura@zscaler.net",
+		"yuta.kawamura@zscaler.net",
+		"yuta.kawamura@zscaler.net",
+		true,
+		"P@ssw0rd",
+		"ORGANIZATION",
+		"Yuta Kawamura",
+	)
+	fmt.Print(adminusers)
+
+	// Show Admin Users
+	// fmt.Println(pkg.FetchAllNetworkServices())
+
+	// Show all url categories name
+	// urlcategories := pkg.FetchAllUrlCategories()
+	// fmt.Print(urlcategories)
+
+	// Show url filtering policies
 	// pkg.FetchAllUrlFilteringRules()
-	// fmt.Print(pkg.FetchAllUrlCategories())
+
+	// Show url lookup information
 	// target_urls := []string{"aaa.com", "softbank.com"}
 	// category := pkg.LookupUrlCategory(target_urls)
 	// fmt.Print(category)
-	// fmt.Println(pkg.FetchAllNetworkServices())
 }

c8b8be935587a4a46008623426e5c225b6baffb6
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2022-04-20 07:28:50 +0900

    improve go version

--- go.mod
+++ go.mod
@@ -1,6 +1,8 @@
 // module github.com/yuta519/zscaler_golang
 module zscaler_golang
 
-go 1.16
+go 1.18
 
-require gopkg.in/ini.v1 v1.62.0 // indirect
+require gopkg.in/ini.v1 v1.62.0
+
+require github.com/smartystreets/goconvey v1.7.2 // indirect
--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"zscaler_golang/pkg"
 )
 
@@ -29,45 +30,45 @@ func main() {
 	// pkg.FetchAllUrlFilteringRules()
 
 	// Show url lookup information
-	// target_urls := []string{"aaa.com", "softbank.com"}
-	// category := pkg.LookupUrlCategory(target_urls)
-	// fmt.Print(category)
+	target_urls := []string{"aaa.com", "softbank.com"}
+	category := pkg.LookupUrlCategory(target_urls)
+	fmt.Print(category)
 
 	// Create New URL Filtering Rule
-	pkg.CreateUrlFilteringRule(
-		"READ_WRITE",
-		"TEST KAWAMURA RULE",
-		1,
-		[]string{
-			"DOHTTPS_RULE",
-			"TUNNELSSL_RULE",
-			"HTTP_PROXY",
-			"FOHTTP_RULE",
-			"FTP_RULE",
-			"HTTPS_RULE",
-			"HTTP_RULE",
-			"SSL_RULE",
-			"TUNNEL_RULE",
-		},
-		nil,
-		[]string{"CUSTOM_01", "CUSTOM_02"},
-		"ENABLED",
-		0,
-		[]string{
-			"OPTIONS",
-			"GET",
-			"HEAD",
-			"POST",
-			"PUT",
-			"DELETE",
-			"TRACE",
-			"CONNECT",
-			"OTHER",
-		},
-		false,
-		false,
-		0,
-		"ALLOW",
-	)
+	// pkg.CreateUrlFilteringRule(
+	// 	"READ_WRITE",
+	// 	"TEST KAWAMURA RULE",
+	// 	1,
+	// 	[]string{
+	// 		"DOHTTPS_RULE",
+	// 		"TUNNELSSL_RULE",
+	// 		"HTTP_PROXY",
+	// 		"FOHTTP_RULE",
+	// 		"FTP_RULE",
+	// 		"HTTPS_RULE",
+	// 		"HTTP_RULE",
+	// 		"SSL_RULE",
+	// 		"TUNNEL_RULE",
+	// 	},
+	// 	nil,
+	// 	[]string{"CUSTOM_01", "CUSTOM_02"},
+	// 	"ENABLED",
+	// 	0,
+	// 	[]string{
+	// 		"OPTIONS",
+	// 		"GET",
+	// 		"HEAD",
+	// 		"POST",
+	// 		"PUT",
+	// 		"DELETE",
+	// 		"TRACE",
+	// 		"CONNECT",
+	// 		"OTHER",
+	// 	},
+	// 	false,
+	// 	false,
+	// 	0,
+	// 	"ALLOW",
+	// )
 
 }

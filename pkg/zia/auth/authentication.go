ae201a7986aa1b88416422cb68a08a48a86757ff
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2022-04-22 09:03:33 +0900

    simplify auth

--- pkg/zia/auth/authentication.go
+++ pkg/zia/auth/authentication.go
@@ -7,8 +7,8 @@ import (
 	"net/http"
 	"net/url"
 	"strconv"
+	"time"
 
-	"zscaler_golang/auth"
 	"zscaler_golang/pkg/zia/config"
 )
 
@@ -19,6 +19,38 @@ type ApiCredential struct {
 	Timestamp string `json:"timestamp"`
 }
 
+type AuthPrepare struct {
+	ObfuscatedApiKey string
+	Timestamp        int
+}
+
+var auth AuthPrepare
+
+func init() {
+	unix_now := time.Now().UnixNano() / int64(time.Millisecond)
+	timestamp := int(unix_now)
+	convert_str_unix := strconv.FormatInt(unix_now, 10)
+	key_from_unix := convert_str_unix[len(convert_str_unix)-6:]
+	r, _ := strconv.Atoi(key_from_unix)
+	shifted_key := fmt.Sprintf("%06d", r>>1)
+
+	apikey := config.Config.ApiKey
+	var obfuscatedApiKey string
+	for _, i := range key_from_unix {
+		index, _ := strconv.Atoi(string(i))
+		obfuscatedApiKey += string(apikey[index])
+	}
+	for _, i := range shifted_key {
+		index, _ := strconv.Atoi(string(i))
+		obfuscatedApiKey += string(apikey[index+2])
+	}
+
+	auth = AuthPrepare{
+		ObfuscatedApiKey: obfuscatedApiKey,
+		Timestamp:        timestamp,
+	}
+}
+
 func Login() string {
 	var session_id string
 
@@ -26,10 +58,10 @@ func Login() string {
 	reference, _ := url.Parse("/api/v1/authenticatedSession")
 	endpoint := base.ResolveReference(reference).String()
 	payload := new(ApiCredential)
-	payload.APIKey = auth.Auth.ObfuscatedApiKey
+	payload.APIKey = auth.ObfuscatedApiKey
 	payload.Username = config.Config.UserName
 	payload.Password = config.Config.Password
-	payload.Timestamp = strconv.Itoa(auth.Auth.Timestamp)
+	payload.Timestamp = strconv.Itoa(auth.Timestamp)
 	payload_json, _ := json.Marshal(payload)
 
 	res, err := http.Post(endpoint, "application/json", bytes.NewBuffer(payload_json))

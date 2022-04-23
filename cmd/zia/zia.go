3e066b26338f96b0814a2854efe5f9ff1ae7b1b2
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2022-04-23 17:27:03 +0900

    add cli options at main

---
+++ cmd/zia/zia.go
@@ -0,0 +1,163 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+
+	"zscaler_golang/pkg/zia/admin"
+	"zscaler_golang/pkg/zia/config"
+)
+
+const (
+	name     = "zia_golang"
+	version  = "0.0.1"
+	revision = "HEAD"
+)
+
+func usage() {
+	fmt.Fprint(os.Stderr, `Tasks:
+	zia adminuser [TASK]                # Show adminusers in Zscaler Internet Access
+	zia adminrole [TASK]                # Show adminroles in Zscaler Internet Access
+Options:
+`)
+
+	// 	fmt.Fprint(os.Stderr, `Tasks:
+	//   goreman check                      # Show entries in Procfile
+	//   goreman help [TASK]                # Show this help
+	//   goreman export [FORMAT] [LOCATION] # Export the apps to another process
+	//                                        (upstart)
+	//   goreman run COMMAND [PROCESS...]   # Run a command
+	//                                        start
+	//                                        stop
+	//                                        stop-all
+	//                                        restart
+	//                                        restart-all
+	//                                        list
+	//                                        status
+	//   goreman start [PROCESS]            # Start the application
+	//   goreman version                    # Display Goreman version
+	// Options:
+	// `)
+	flag.PrintDefaults()
+	os.Exit(0)
+}
+
+type conf struct {
+	UserName string
+	Password string
+	Hostname string
+	ApiKey   string
+	Args     []string
+}
+
+func readConfg() *conf {
+	var cfg conf
+
+	flag.Parse()
+	if flag.NArg() == 0 {
+		usage()
+	}
+
+	cfg.UserName = config.Config.UserName
+	cfg.Password = config.Config.Password
+	cfg.Hostname = config.Config.Hostname
+	cfg.ApiKey = config.Config.ApiKey
+	cfg.Args = flag.Args()
+
+	return &cfg
+}
+
+func main() {
+	var err error
+	if err != nil {
+		fmt.Fprint(os.Stderr, "zia: ", err.Error())
+	}
+	cfg := readConfg()
+
+	cmd := cfg.Args[0]
+	switch cmd {
+	case "p":
+		fmt.Print("hoge")
+	case "adminuser":
+		if len(cfg.Args) < 2 {
+			fmt.Fprint(os.Stderr, "adminuser: Please specify sub command\n")
+			os.Exit(1)
+		}
+		if cfg.Args[1] == "ls" {
+			fmt.Println(admin.FetchAllAdminUsers())
+		}
+	}
+}
+
+// fmt.Print(cmd)
+// fmt.Print(a)
+// fmt.Print(&a)
+// fmt.Print(*a)
+// Create New Admin Users
+// adminusers := pkg.CreateAdminUsers(
+// 	"Super Admin",
+// 	"yuta.kawamura@zscaler.net",
+// 	"yuta.kawamura@zscaler.net",
+// 	"yuta.kawamura@zscaler.net",
+// 	true,
+// 	"P@ssw0rd",
+// 	"ORGANIZATION",
+// 	"Yuta Kawamura",
+// )
+// fmt.Print(adminusers)
+
+// Show Admin Users
+// fmt.Println(admin.FetchAllAdminRoles())
+// readConfg()
+// fmt.Println(admin.FetchAllAdminUsers())
+
+// Show all url categories name
+// urlcategories := pkg.FetchAllUrlCategories()
+// fmt.Print(urlcategories)
+
+// Show url filtering policies
+// pkg.FetchAllUrlFilteringRules()
+
+// Show url lookup information
+// target_urls := []string{"aaa.com", "softbank.com"}
+
+// category := urlcategory.LookupUrlCategory(target_urls)
+// fmt.Print(category)
+
+// Create New URL Filtering Rule
+// pkg.CreateUrlFilteringRule(
+// 	"READ_WRITE",
+// 	"TEST KAWAMURA RULE",
+// 	1,
+// 	[]string{
+// 		"DOHTTPS_RULE",
+// 		"TUNNELSSL_RULE",
+// 		"HTTP_PROXY",
+// 		"FOHTTP_RULE",
+// 		"FTP_RULE",
+// 		"HTTPS_RULE",
+// 		"HTTP_RULE",
+// 		"SSL_RULE",
+// 		"TUNNEL_RULE",
+// 	},
+// 	nil,
+// 	[]string{"CUSTOM_01", "CUSTOM_02"},
+// 	"ENABLED",
+// 	0,
+// 	[]string{
+// 		"OPTIONS",
+// 		"GET",
+// 		"HEAD",
+// 		"POST",
+// 		"PUT",
+// 		"DELETE",
+// 		"TRACE",
+// 		"CONNECT",
+// 		"OTHER",
+// 	},
+// 	false,
+// 	false,
+// 	0,
+// 	"ALLOW",
+// )
